11e5ec08635040b82b29d2d50faffa8150e2626a
Miguel Santos <miggsantos@gmail.com>
2015-10-15 03:34:25 +0100

    main and detail page layout finished

--- pokedex/PokemonDetailVC.swift
+++ pokedex/PokemonDetailVC.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
+    
+    @IBOutlet weak var nameLbl: UILabel!
+
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+
+    var pokemon: Pokemon!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        nameLbl.text = pokemon.name
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +51,8 @@ class PokemonDetailVC: UIViewController {
     }
     */
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
