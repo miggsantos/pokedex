10ac84e1e64ca6037e29fa342e1431e0995d78ed
Miguel Santos <miggsantos@gmail.com>
2015-10-15 03:31:48 +0100

    main and detail page layout finished

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Miguel Santos on 14/10/15.
+//  Copyright © 2015 Miguel Santos. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Miguel Santos on 13/10/15.
+//  Copyright © 2015 Miguel Santos. All rights reserved.
+//
+
+import Foundation
